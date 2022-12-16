2faa6aa609dad4821fa64548c5ee0f43342d1197
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-12-16 11:07:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ICS2O-UNIT5-07-CSharp-REPEATED-ADDITION
+
+[![Mr Coxall's Super Linter](https://github.com/Alex-Nelson-1/ICS2O-UNIT5-07-CSharp-REPEATED-ADDITION/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Alex-Nelson-1/ICS2O-UNIT5-07-CSharp-REPEATED-ADDITION/actions/)
+
+[![Run on Repl.it](https://repl.it/badge/github/Alex-Nelson-1/ICS2O-UNIT5-07-CSharp-REPEATED-ADDITION)](https://repl.it/github/Alex-Nelson-1/ICS2O-UNIT5-07-CSharp-REPEATED-ADDITION)
---
+++ main.cs
@@ -0,0 +1,39 @@
+// Created by: Alex Nelson
+// Created on: Nov 2022
+//
+// This program tells you which age range of movies you can
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Explanation of the algorithm
+        Console.WriteLine("Students between 12 and 21 receive a discount to the museum on weekdays. Enter in your age and the day you're visiting the museum below ");
+        Console.WriteLine("");
+        Console.WriteLine("Please enter your age");
+        //Input for user’s age
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");
+        Console.WriteLine("Please enter the day as a number(Monday being 1, and Sunday being 7)");
+        //Input for the day of the week
+        int day = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");
+        //Process: Function to tell the user to restart if the number is more than 7
+        if (day > 7)
+        {
+            Console.WriteLine("You have input something incorrectly. Please relaunch code.");
+        }
+        else if ((age > 12 && age < 21) && (day != 6 && day != 7))
+        {
+            //If eligible output “Eligible”
+            Console.WriteLine("Eligible!");
+        }
+        //If not eligible output “Not Eligible”
+        else
+        {
+            Console.WriteLine("Not Eligible!");
+        }
+        Console.WriteLine("\nDone");
+    }
+}
